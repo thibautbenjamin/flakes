3f99ed45669e42ebe6014518e91bed35d86025cf
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2026-09-01 17:02:26 +0200

    lean4-mode

---
+++ pkgs/lean4-mode/default.nix
@@ -0,0 +1,20 @@
+{
+  melpaBuild,
+  fetchFromGitHub,
+}:
+melpaBuild rec {
+    pname = "lean4-mode";
+    version = "1";
+    commit = "37d5c99b7b29c80ab78321edd6773200deb0bca6";
+    src = fetchFromGitHub {
+      owner = "leanprover";
+      repo = "lean4-mode";
+      rev = commit;
+      sha256 = "sha256-+dRaXB7uvN/weSZiKcfSKWhcdJVNg9Vg8k0pJkDNjpc=";
+    };
+    packageRequires = with melpaPackages;
+      [ dash f flycheck magit-section lsp-mode s ];
+    recipe = self.writeText "recipe" ''
+               (lean4-mode :repo "leanprover/lean4-mode" :fetcher github)
+             '';
+}
